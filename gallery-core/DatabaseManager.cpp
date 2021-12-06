11905cfffb30539e007134550677b3ed89548544
Shashwat <svangani239@gmail.com>
2021-12-06 20:35:30 +0530

    Loading a database on mobile devices
    - Updated DatabaseManager::instance() function in gallery-core library
    to properly create database for mobile devices (android/ios).
    - We updated database so that the application has proper permissions to
    instantiate SqlLite3 database.
    - To get specific details on how it is done, refer page 171.

--- gallery-core/DatabaseManager.cpp
+++ gallery-core/DatabaseManager.cpp
@@ -17,9 +17,28 @@ void DatabaseManager::debugQuery(const QSqlQuery& query)
 
 DatabaseManager&DatabaseManager::instance()
 {
-    static DatabaseManager singleton;
-    return singleton;
+    #if defined(Q_OS_ANDROID) || defined(Q_OS_IOS)
+        QFile assetDbFile(":/database/" + DATABASE_FILENAME);
+        QString destinationDbFile =
+            QStandardPaths::writableLocation(QStandardPaths::AppLocalDataLocation)
+            .append("/" + DATABASE_FILENAME);
+        qDebug() << "Assets file" << assetDbFile.fileName();
+        qDebug() << "Database file" << destinationDbFile;
+        if (assetDbFile.exists()) {
+            if (!QFile::exists(destinationDbFile)) {
+                assetDbFile.copy(destinationDbFile);
+                QFile::setPermissions(destinationDbFile,
+                    QFile::WriteOwner | QFile::ReadOwner);
+                qDebug() << "Setted permissions on copied file";
+            }
+        }
+        static DatabaseManager singleton(destinationDbFile);
+    #else
+        static DatabaseManager singleton;
+    #endif
+        return singleton;
 }
+
 
 DatabaseManager::DatabaseManager(const QString& path) :
     mDatabase(new QSqlDatabase(QSqlDatabase::addDatabase("QSQLITE"))),
